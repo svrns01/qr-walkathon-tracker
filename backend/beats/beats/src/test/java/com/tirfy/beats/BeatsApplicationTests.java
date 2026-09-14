64843769c5136a07c826291a9437a7a11bd64647
Venkat <venkatnaralas@gmail.com>
2026-09-14 19:32:01 +0530

    backend added

---
+++ backend/beats/beats/src/test/java/com/tirfy/beats/BeatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tirfy.beats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
